28c6fcb17dd74a8cbcf05d3de40297f709f6264a
VelazqueZzz <93205261+VelazqueZzz@users.noreply.github.com>
2024-11-11 22:59:23 +0300

    Add files via upload
    //http://localhost:8080/home?color=blue&name=Jane

--- src/main/java/org/example/firstdemo/MainController.java
+++ src/main/java/org/example/firstdemo/MainController.java
@@ -3,12 +3,18 @@ package org.example.firstdemo;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
-    @RequestMapping("/home")
-    public String home() {
+    @RequestMapping("/home")                                //http://localhost:8080/home?color=blue&name=Jane
+    public String home(
+        @RequestParam (required = false)String name,
+        @RequestParam (required = false)String color, Model page){
+        page.addAttribute("username", name);
+        page.addAttribute("color", color);
         return "home.html";
     }
 }
